ff7831d86d9896be77357cb190c3e6d8a0698851
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-05-24 16:03:46 -0300

    feat(Races) Criado a classe Races com seus mótodos e atributos

---
+++ src/Races/Races.ts
@@ -0,0 +1,19 @@
+abstract class Races {
+  constructor(private _name: string, private _dexterity: number) { }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get dexterity(): number {
+    return this._dexterity;
+  }
+
+  static createdRacesInstances() {
+    throw new Error('Not implemented');
+  }
+
+  abstract get maxLifePoints(): number;
+}
+
+export default Races;
